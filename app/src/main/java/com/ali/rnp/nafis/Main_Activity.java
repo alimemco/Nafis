51a90443c375d4df16cabad151d962ddb6e6e422
alimemco <alimoghadam75@gmail.com>
2018-11-11 16:54:33 +0330

    BottomNavigation

--- app/src/main/java/com/ali/rnp/nafis/Main_Activity.java
+++ app/src/main/java/com/ali/rnp/nafis/Main_Activity.java
@@ -1,13 +1,23 @@
 package com.ali.rnp.nafis;
 
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main_Activity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        bottomNavigationView = findViewById(R.id.mainActivity_BottomNavigation);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_navigation_home);
     }
 }
