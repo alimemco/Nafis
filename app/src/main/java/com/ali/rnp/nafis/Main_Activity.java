1d29605021ee7652ca87dffc009f1f6de3945cd2
alimemco <alimoghadam75@gmail.com>
2018-11-11 14:42:19 +0330

    Initial commit

---
+++ app/src/main/java/com/ali/rnp/nafis/Main_Activity.java
@@ -0,0 +1,13 @@
+package com.ali.rnp.nafis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
