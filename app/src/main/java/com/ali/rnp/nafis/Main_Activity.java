1326b60e64e234e0693c9cb4be9fbe635ff8dad4
alimemco <alimoghadam@gmail.com>
2018-11-12 01:33:35 +0330

    Toolbar

--- app/src/main/java/com/ali/rnp/nafis/Main_Activity.java
+++ app/src/main/java/com/ali/rnp/nafis/Main_Activity.java
@@ -1,8 +1,13 @@
 package com.ali.rnp.nafis;
 
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class Main_Activity extends AppCompatActivity {
 
@@ -14,6 +19,26 @@ public class Main_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initViews();
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.maintoolbar);
+        DrawerLayout drawerLayout = findViewById(R.id.main_drawer_layout);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.White));
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(
+                this,drawerLayout,toolbar,0,0 );
+
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
     }
 
     private void initViews() {
