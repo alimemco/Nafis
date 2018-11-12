2a0d4be03f98a92aebbb22d035dade6d29a6a183
alimemco <44494398+alimemco@users.noreply.github.com>
2018-11-13 00:00:23 +0330

    Update Main_Activity.java

--- app/src/main/java/com/ali/rnp/nafis/Main_Activity.java
+++ app/src/main/java/com/ali/rnp/nafis/Main_Activity.java
@@ -21,6 +21,7 @@ public class Main_Activity extends AppCompatActivity {
         initViews();
         setupToolbar();
         //fetch
+        //update with version controller stash
     }
 
     private void setupToolbar() {
