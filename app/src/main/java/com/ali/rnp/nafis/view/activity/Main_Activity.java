5fd85bf816203807f138eac004dad8877a566e84
alimemco <alimoghadam@gmail.com>
2018-11-13 21:35:44 +0330

    Toolbar Items

--- app/src/main/java/com/ali/rnp/nafis/view/activity/Main_Activity.java
+++ app/src/main/java/com/ali/rnp/nafis/view/activity/Main_Activity.java
@@ -36,7 +36,7 @@ public class Main_Activity extends AppCompatActivity {
         setupToolbar();
         setupFragments();
         setupBottomNavigation();
-        //changed
+        //changedLast
 
 
     }
